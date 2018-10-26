32334d592f14b02102ebec056b1f79d7538b81d2
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 19:57:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num == 1 || num < 0
+    return false
+  elsif num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0
+    return true
+  end
+end
+  
