68bc91d2287e3f79499d9eb683e02b8927bbf2ab
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:10:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(num)
+  if num <= 1
+    return false
+  end
   for d in 2..(num - 1)
-    if (num % d) == 0 || num <= 1
+    if (num % d) == 0
       return false
     end
   end
